131f23f9052473c044a68f35af8310bae4337818
[mahapatrasonali] <[sonangr2000@gmail.com]>
2022-12-27 15:38:00 +0530

    Find

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        // public static void toPrint<T>(T[] inputArray)
+        // {
+        //    foreach (var element in inputArray)
+        //  {
+        //         Console.WriteLine(element);
+        //     }
+        //  }
+        // public static void toPrint(double[] inputarray)
+        // {
+        //  foreach (double element in inputArray)
+        //  {
+        //    Console.WriteLine(element);
+        // }
+        // }
+        // public static void toPrint(char[] inputArray)
+        //{
+        // foreach (char element in inputArray)
+        // }
+        //  Console.WriteLine(element);
+        //}
+
+        public static void Main(string[] args)
+        {
+          //  int[] intArray = { 1, 2, 3, 4, 5 };
+          //  double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+          //  char[] charArray = { 'H', 'E', 'A', 'R' };
+
+            // toPrint<int>(intArray);
+            // toPrint<double>(doubleArray);
+            // toPrint<char>(charArray);
+
+            //   PrintArray<int> intprintArray = new PrintArray<int>(intArray);
+            // intprintArray.toPrint();
+            //PrintArray<double> doubleprintArray = new PrintArray<double>(doubleArray);
+            //doubleprintArray.toPrint();
+            // PrintArray<char> charprintArray = new PrintArray<char>(charArray);
+            //charprintArray.toPrint();
+            //Console.ReadKey();
+
+            int num1 = 34;
+            int num2 = 20;
+            
+            
+            
+            int value = num1.CompareTo(num2);
+            
+
+             Console.WriteLine(value);
+            Console.ReadKey();
+        }
+
+    }
+}
