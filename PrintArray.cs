c4631c2ce65273760d803850273335bc19ce91c0
[mahapatrasonali] <[sonangr2000@gmail.com]>
2022-12-27 15:41:07 +0530

    Maximum Number

---
+++ PrintArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    public class PrintArray<T>
+    {
+        private T[] inputarray;
+
+        public PrintArray(T[] inputArray)
+        {
+            this.inputarray = inputArray;
+        }
+
+        public void toPrint()
+        {
+            foreach (var element in inputarray)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+    }
+}
